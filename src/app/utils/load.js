6a4805806a0dba539142e59df2efdc9a1baacca6
ianvei <iamianveilleux@gmail.com>
2022-05-18 13:28:13 -0400

    added webpack

---
+++ src/app/utils/load.js
@@ -0,0 +1,13 @@
+// let loadWepbage = function(){
+//     return console.log('I am loading')
+// }
+
+export function loadWebpage(){
+    let page = document.querySelector('.page')
+    let attempt = document.getElementById('content')
+    console.log(attempt)
+    console.log(page)
+    // page.remove()
+    return console.log('I am loading')
+
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 console.log('Parker')
 console.log('tarker')
+import { loadWebpage } from './app/utils/load.js'
+
+loadWebpage()
